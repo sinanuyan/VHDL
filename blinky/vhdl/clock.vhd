53745d1262f320b6ed1f5dbf890d67e7f353a008
sinanuyan <sinan.uyan@gmail.com>
2021-02-27 23:18:43 +0100

    add icestick config file

--- blinky/vhdl/clock.vhd
+++ blinky/vhdl/clock.vhd
@@ -9,19 +9,19 @@ entity clock is
 end clock;
 
 architecture rtl of clock is
-    constant c_CNT_1HZ   : natural := 6000000;
-    signal r_CNT_1HZ   : natural range 0 to c_CNT_1HZ;
-    signal r_TOGGLE_1HZ   : std_logic := '0';
+    constant c_CNT_1HZ      : natural := 6000000;
+    signal r_CNT_1HZ        : natural range 0 to c_CNT_1HZ;
+    signal r_TOGGLE_1HZ     : std_logic := '0';
 begin
     p_clk_1hz : process (clk_pi) is
     begin
         if rising_edge(clk_pi) then
-        if r_CNT_1HZ = c_CNT_1HZ-1 then  -- -1, since counter starts at 0
-            r_TOGGLE_1HZ <= not r_TOGGLE_1HZ;
-            r_CNT_1HZ    <= 0;
-        else
-            r_CNT_1HZ <= r_CNT_1HZ + 1;
-        end if;
+            if r_CNT_1HZ = c_CNT_1HZ-1 then  -- -1, since counter starts at 0
+                r_TOGGLE_1HZ <= not r_TOGGLE_1HZ;
+                r_CNT_1HZ    <= 0;
+            else
+                r_CNT_1HZ <= r_CNT_1HZ + 1;
+            end if;
         end if;
     end process p_clk_1hz;
     clk_1hz <= r_TOGGLE_1HZ;
