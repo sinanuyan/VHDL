da55ed7a9c67688f9e2e9dfcd438ff71b472b851
sinanuyan <sinan.uyan@gmail.com>
2021-03-06 22:39:47 +0100

    add dutycycle block

---
+++ Lattice_iCEstick/pwm/vhdl/duty_cycle.vhd
@@ -0,0 +1,33 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity duty_cycle is
+    port(
+        clk_pi          : in  std_logic;
+        rst_pi          : in std_logic;
+        duty_cylce_po   : out std_logic_vector(4 downto 0)
+    );
+end duty_cycle;
+
+architecture rtl of duty_cycle is
+    constant c_CNT_100HZ        : natural := 600000;
+    signal r_CNT_100HZ          : natural range 0 to c_CNT_100HZ;
+    signal duty_cycle           : unsigned(4 downto 0);
+begin
+    duty_p: process(clk_pi, rst_pi)
+    begin
+        if rst_pi = '1' then
+            r_CNT_100HZ <= 0;
+            duty_cycle <= (others => '0');   
+        elsif rising_edge(clk_pi) then
+            if r_CNT_100HZ = c_CNT_100HZ - 1 then              
+                duty_cycle <= duty_cycle + 1;
+                r_CNT_100HZ <= 0;
+            else
+                r_CNT_100HZ <= r_CNT_100HZ + 1;   
+            end if;
+        end if;
+    end process duty_p;
+    duty_cylce_po <= std_logic_vector(duty_cycle);
+end rtl;
--- Lattice_iCEstick/pwm/vhdl/pwm.vhd
+++ Lattice_iCEstick/pwm/vhdl/pwm.vhd
@@ -5,25 +5,41 @@ use ieee.numeric_std.all;
 
 entity pwm is
     port(
-        clk: in  std_logic;
+        clk_pi: in  std_logic;
         rst_pi : in std_logic;
-        led_out: out std_logic
+        led_out: out std_logic(4 downto 0);
     );
 end pwm;
 
 
 architecture rtl of pwm is
-    constant duty_cycle     : natural := 8;
+    component duty_cycle is
+        port(
+            clk_pi          : in  std_logic;
+            rst_pi          : in std_logic;
+            duty_cylce_po   : out std_logic_vector(4 downto 0)
+            );
+    end component duty_cycle;
+
+    signal dc               : std_logic_vector(4 downto 0);
     signal pwm_counter      : unsigned(3 downto 0);
     signal pwm_out          : std_logic := '0';
 begin
-    pwm_p: process(clk, rst_pi)
+    DutyCycle : duty_cycle
+        port map(
+            clk_pi => clk_pi,
+            rst_pi => rst_pi,
+            duty_cylce_po => dc
+        );
+
+
+    pwm_p: process(clk_pi, rst_pi)
     begin
         if rst_pi = '1' then
             pwm_counter <= (others => '0');
             pwm_out <= '0';     
-        elsif rising_edge(clk) then
-            if pwm_counter < duty_cycle then
+        elsif rising_edge(clk_pi) then
+            if pwm_counter < unsigned(dc) then
                 pwm_out <= '1';
             else
                 pwm_out <= '0';
@@ -31,5 +47,6 @@ begin
             pwm_counter <= pwm_counter + 1;
         end if;
     end process pwm_p;
+    
     led_out <= pwm_out;
 end rtl;
