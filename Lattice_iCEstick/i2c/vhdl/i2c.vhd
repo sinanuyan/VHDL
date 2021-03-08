d92682943331977538b056803ed5721483e7d971
sinanuyan <sinan.uyan@gmail.com>
2021-03-08 22:15:22 +0100

    add i2c master file

---
+++ Lattice_iCEstick/i2c/vhdl/i2c.vhd
@@ -0,0 +1,15 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+
+entity i2c_master is
+    port(
+        clk: in  std_logic;
+        rst_pi : in std_logic
+    );
+end i2c_master;
+
+architecture rtl of i2c_master is
+begin
+end rtl;
